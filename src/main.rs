d839a24d589764f8af2230111ca666ed488af2d4
aeldar <eldar.aliyev8@gmail.com>
2020-01-01 22:50:08 +0200

    add yaml-rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+use yaml_rust::YamlLoader;
+
 fn main() {
     println!("Hello, world!");
+
+    let content = fs::read_to_string("shrapnel.yml").expect("cannot read the file");
+
+    println!("Content:\n{}", content);
+
+    let parsed_content = YamlLoader::load_from_str(&content);
+
+    println!(
+        "Parsed content:\n{:?}",
+        parsed_content.unwrap()[0]["fragments"]
+    );
 }

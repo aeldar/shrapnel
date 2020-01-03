9df6736a54aa8a178eedb4afa20102dfe9d475d9
aeldar <eldar.aliyev8@gmail.com>
2020-01-03 17:55:50 +0200

    use index as default name

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
+use std::env;
 use std::fs;
 use std::process::{Command, Stdio};
 use yaml_rust::YamlLoader;
 
 fn main() {
-    println!("Hello, world!");
-
     let content = fs::read_to_string("shrapnel.yml").expect("cannot read the file");
-
-    println!("Content:\n{}", content);
 
     let parsed_content = YamlLoader::load_from_str(&content).unwrap();
 
     let fragments = &parsed_content[0]["fragments"];
 
-    println!("Parsed content:\n{:?}", fragments);
-
     for (index, fragment) in fragments.as_vec().unwrap().iter().enumerate() {
-        println!("Hello: {:?}, {:?}", index, fragment);
-
-        let name = &fragment["name"].as_str().unwrap_or("1234");
+        let _name = &fragment["name"]
+            .as_str()
+            .unwrap_or(&format!("{}", index)[..]);
         let dir = &fragment["dir"].as_str().unwrap_or(".");
-        let cmd = &fragment["cmd"].as_str().unwrap_or("");
-
-        println!("Name: {:?}", name);
-        println!("Dir: {:?}", dir);
-        println!("Cmd: {:?}", cmd);
+        let cmd = &fragment["cmd"].as_str().unwrap_or("ls");
 
         create_command(cmd)
+            .envs(env::vars())
             .current_dir(dir)
             .stdin(Stdio::null())
             .stdout(Stdio::inherit())

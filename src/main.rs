366b6f6a10bfddc7578548621ac8d535fdecef98
aeldar <eldar.aliyev8@gmail.com>
2020-01-03 17:37:18 +0200

    run command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::process::{Command, Stdio};
 use yaml_rust::YamlLoader;
 
 fn main() {
@@ -8,10 +9,40 @@ fn main() {
 
     println!("Content:\n{}", content);
 
-    let parsed_content = YamlLoader::load_from_str(&content);
+    let parsed_content = YamlLoader::load_from_str(&content).unwrap();
 
-    println!(
-        "Parsed content:\n{:?}",
-        parsed_content.unwrap()[0]["fragments"]
-    );
+    let fragments = &parsed_content[0]["fragments"];
+
+    println!("Parsed content:\n{:?}", fragments);
+
+    for (index, fragment) in fragments.as_vec().unwrap().iter().enumerate() {
+        println!("Hello: {:?}, {:?}", index, fragment);
+
+        let name = &fragment["name"].as_str().unwrap_or("1234");
+        let dir = &fragment["dir"].as_str().unwrap_or(".");
+        let cmd = &fragment["cmd"].as_str().unwrap_or("");
+
+        println!("Name: {:?}", name);
+        println!("Dir: {:?}", dir);
+        println!("Cmd: {:?}", cmd);
+
+        create_command(cmd)
+            .current_dir(dir)
+            .stdin(Stdio::null())
+            .stdout(Stdio::inherit())
+            .spawn()
+            .expect("Something went wrong, sorry");
+    }
+}
+
+fn create_command(command: &str) -> Command {
+    let words: Vec<&str> = command.split_whitespace().collect();
+    let fst = words[0];
+    let args = &words[1..];
+
+    let mut cmd = Command::new(fst);
+
+    cmd.args(args);
+
+    cmd
 }
